1b90c3971d0d48ff854d444a4b56ce2be7a3fe58
nxkxxx <nonedity@gmail.com>
2026-03-29 14:55:10 +0800

    Adding v-bind in Footer section, changing the template in Main section and deleting the buttons

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,38 +1,41 @@
 <script setup>
 import { ref } from 'vue'
 
-const paragraf1 = ref("I am incredibly small and fast! My core library is only around 30KB, so I won't slow you down.")
-const paragraf2 = ref("Easy to learn and use, even for beginners. I have a gentle learning curve, clear documentation, and a supportive community.")
-const paragraf3 = ref("I can handle everything from simple interactive elements to complex single-page applications. I'm great for small projects and large - scale applications alike.")
-
-const href = ref("https://instagram.com/nonedity")
-const isBtnDisabled = ref(true)
+const facts = ref([
+    {
+        "adjective": "Lightweight",
+        "description": "I am incredibly small and fast! My core library is only around 30KB, so I won't slow you down."
+    },
+    {
+        "adjective": "Approachable",
+        "description": "Easy to learn and use, even for beginners. I have a gentle learning curve, clear documentation, and a supportive community."
+    },
+    {
+        "adjective": "Versatile",
+        "description": "I can handle everything from simple interactive elements to complex single-page applications. I'm great for small projects and large - scale applications alike."
+    }
+])
 </script>
 
 <template>
     <main>
         <section>
             <h2>
-                I'm <span class="highlight">Lightweight</span>
+                I'm <span class="highlight">{{ facts[0].adjective }}</span>
             </h2>
-            <p>{{ paragraf1 }}</p>
+            <p>{{ facts[0].description }}</p>
         </section>
         <section>
             <h2>
-                I'm <span class="highlight">Approachable</span>
+                I'm <span class="highlight">{{ facts[1].adjective }}</span>
             </h2>
-            <p>{{ paragraf2 }}</p>
+            <p>{{ facts[1].description }}</p>
         </section>
         <section>
             <h2>
-                I'm <span class="highlight">Versatile</span>
+                I'm <span class="highlight">{{ facts[2].adjective }}</span>
             </h2>
-            <p>{{ paragraf3 }}</p>
-
-            <div class="buttons">
-                <button target="__blank" :href>My Instagram!</button>
-                <button :disabled="isBtnDisabled">This is a disabled button.</button>
-            </div>
+            <p>{{ facts[2].description }}</p>
         </section>
     </main>
 </template>
@@ -55,33 +58,5 @@ h2 {
 p {
     font-size: 0.9rem;
     color: #555;
-}
-
-.buttons {
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-}
-
-button {
-    background: #34495e;
-    color: white;
-    padding: 10px;
-    border: 0;
-    font-size: 1.1rem;
-    border-radius: 0 5px 0 5px;
-    font-weight: bold;
-    margin: 15px 15px 0 0;
-    cursor: pointer;
-    transition: transform 0.2s;
-}
-
-button:hover {
-    transform: scale(1.05);
-}
-
-button:disabled {
-    background: grey;
-    cursor: not-allowed;
-    opacity: 0.5;
 }
 </style>
